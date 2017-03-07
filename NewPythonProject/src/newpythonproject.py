9a8737b5d1eed533971c0fe918d3521142de865a
HosYuta <n01001@10.1.108.224>
2017-03-07 10:44:26 +0900

    first commit

---
+++ NewPythonProject/src/newpythonproject.py
@@ -0,0 +1,14 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "wom"
+__date__ = "$2017/03/07 10:38:12$"
+
+if __name__ == "__main__":
+    print "Hello World"
+
+
+
+
+
